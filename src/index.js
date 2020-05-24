194102966bb9cd2940a0a433245c514c43e340b4
Samuel Garcia <32781064+MrZamii@users.noreply.github.com>
2020-05-24 15:53:01 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import firebase from "firebase";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-firebase.initializeApp({
-  apiKey: "AIzaSyBQUWl1U4c8e9QtMwsrPk4vxrkbn4dngtQ",
-  authDomain: "proyectoliteratura-72052.firebaseapp.com",
-  databaseURL: "https://proyectoliteratura-72052.firebaseio.com",
-  projectId: "proyectoliteratura-72052",
-  storageBucket: "proyectoliteratura-72052.appspot.com",
-  messagingSenderId: "1005990284321",
-  appId: "1:1005990284321:web:b57a6383beb291bb22a760",
-  measurementId: "G-R67DV07DX4",
-});
 
 ReactDOM.render(
   <React.StrictMode>
