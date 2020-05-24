50901a3296761fbdfaac58c3c8b1c0303246ec80
Samuel Garcia <32781064+MrZamii@users.noreply.github.com>
2020-05-24 15:53:47 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import React, { Component } from "react";
-import firebase from "firebase";
 import HomeImg from "./BooksImg.svg";
 import "./App.css";
 
 class App extends Component() {
-  constructor() {
-    super();
-    this.state = {
-      user: null,
-      pictures: [],
-    };
-
-    this.handleAuth = this.handleAuth.bind(this);
-    // this.handleLogout = this.handleLogout.bind(this);
-    // this.renderLoginButton = this.renderLoginButton.bind(this);
-    // this.handleUpload = this.handleUpload.bind(this);
-  }
-  handleAuth() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then((result) => console.log(`${result.user.email} ha iniciado sesión`))
-      .catch((error) => console.log(`Error ${error.code}: ${error.message}`));
-  }
 
   render() {
     return (
@@ -34,7 +12,7 @@ class App extends Component() {
             <h1>Comparte los archivos de tus clases</h1>
             <p>El conocimiento deber ser libre</p>
             <div className="buttons">
-              <button onClick={this.handleAuth} id="logIn">
+              <button  id="logIn">
                 Iniciar Sesión
               </button>
               <button id="upLoadFile">
